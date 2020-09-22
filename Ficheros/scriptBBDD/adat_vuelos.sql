4db6edeee28e4d4bb812ac37106e2bbd2e915c77
Javier <javiborreguero96@gmail.com>
2020-09-22 13:31:29 +0200

    Segundo commit  - Inserto el script de la base de datos

---
+++ Ficheros/scriptBBDD/adat_vuelos.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-09-2020 a las 13:28:44
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `adat_vuelos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vuelos`
+--
+
+CREATE TABLE `vuelos` (
+  `Id` int(11) NOT NULL,
+  `Codigo_vuelo` varchar(5) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Origen` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Destino` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Fecha` date DEFAULT NULL,
+  `Hora` varchar(5) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Plazas_totales` int(11) DEFAULT NULL,
+  `Plazas_disponibles` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
