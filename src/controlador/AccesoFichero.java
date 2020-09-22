d11d4e0be95eafb8c8b378bd1922abd730b1f2c0
Javier <javiborreguero96@gmail.com>
2020-09-22 16:56:33 +0200

    El metodo de lectura tiene un bucle infinito que debo eliminar

--- src/controlador/AccesoFichero.java
+++ src/controlador/AccesoFichero.java
@@ -3,6 +3,11 @@
  */
 package controlador;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 import modelo.Vuelos;
@@ -12,11 +17,31 @@ import modelo.Vuelos;
  *
  */
 public class AccesoFichero implements GestorAccesoDatos {
+	File infoVuelos;
+	BufferedReader br;
+
+	public AccesoFichero() {
+		System.out.println("ACCESO A DATOS - FICHEROS DE TEXTO");
+		br = null;
+	}
 
 	@Override
 	public HashMap<String, Vuelos> leerVuelos() {
-
-		// TODO Auto-generated method stub
+		HashMap<String, Vuelos> verInfoVuelos = new HashMap<String, Vuelos>();
+		infoVuelos = new File("Ficheros/datos/Vuelos.txt");
+		try {
+			br = new BufferedReader(new FileReader(infoVuelos));
+			String textoPrincipal = br.readLine();
+			while (textoPrincipal != null) {
+				System.out.println(textoPrincipal);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
